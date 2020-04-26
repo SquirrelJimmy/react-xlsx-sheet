dba66e82097f7b55d7f284fed3cc6308fad8f082
binbin <bin@seeapp.com>
2020-04-26 16:23:13 +0800

    :wrench: (release,sh)

--- release.sh
+++ release.sh
@@ -19,3 +19,16 @@ fi;
 
 echo "Branch is $branch"
 echo "Release as $release"
+
+# Tag prefix
+prefix=""
+
+git pull origin $branch
+echo "Current pull origin $branch."
+
+# Generate version number and tag
+standard-version -r $release --tag-prefix $prefix --infile CHANGELOG.md
+
+git push --follow-tags origin $branch
+
+echo "Release finished."
